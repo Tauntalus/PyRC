deb74e8eb4a7b6fbb1c476cceb4ffd62ca0e57d5
Tauntalus <croomb@spu.edu>
2019-06-05 23:46:49 -0700

    added tiered users to try and clean things up

--- client.py
+++ client.py
@@ -11,4 +11,46 @@ class localClient:
 		self.mode = 0
 		self.realName = ""
 		
-		self.prefix = ""				#default IRC prefix for sending stuff
+		self.prefix = ""
+		return
+	
+	def setNick(self, nick):
+		self.nick = nick
+		return
+	
+	def setUserInfo(self, uname, mode, rname):
+		self.userName = uname
+		self.mode = mode
+		self.realName = rname
+		return
+		
+	def verify(self):
+		if (self.nick is not "") and (self.userName is not ""):
+			return true
+		return false
+	
+class localUser(localClient):
+	def __init__(self, clientBase):
+		self.conn = clientBase.conn
+		self.addr = clientBase.addr
+		
+		self.nick = clientBase.nick
+		self.userName = clientBase.userName
+		self.mode = clientBase.mode
+		self.realName = clientBase.realName
+		
+		self.prefix = self.nick + '!' + self.userName + '@PyRC.com'
+		return
+	
+	def setNick():
+		super().setNick(self, nick)
+		self.prefix = self.nick + '!' + self.userName + '@PyRC.com'
+		return
+	
+	def setUserInfo(self, uname, mode, realName):
+		super().setUserInfo(uname, mode, realName)
+		self.prefix = self.nick + '!' + self.userName + '@PyRC.com'
+		return
+	
+	def verify(self):
+		return False	#already verified, no need to run verification code again
